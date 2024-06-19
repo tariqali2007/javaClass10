08246c84b2010891bcc6c01607b0e1b38d32174a
tariqali2007 <tariqalimahar2007@gmail.com>
2024-06-19 21:25:49 +0500

    javaclass10

---
+++ Class10/style10.js
@@ -0,0 +1,31 @@
+
+//let time = new Date();
+
+//console.log(time);
+// console.log(time.getFullYear());]
+//console.log(time.getMilliseconds());
+// console.log(time.getDate())
+
+// time.setDate(25);
+// time.setMonth(6);
+// console.log(time);
+
+
+let exmday = new Date();
+
+exmday.setDate(25);
+exmday.setMonth(7);
+exmday.setHours(9);
+exmday.setMinutes(30);
+console.log(exmday);
+
+// let hoursElem = document.getElementById("hours");
+// let MinutessElem = document.getElementById("Minutes");
+// let SecondsElem = document.getElementById("Seconds");
+
+// setInterval(function(){
+//     let time = new Date();
+//     hoursElem.innerText = time.getHours();
+//     MinutessElem.innerText = time.getMinutes();
+//     SecondsElem.innerText = time.getSeconds()
+// }, 1000)
